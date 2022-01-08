af4c77fd89ff3b42171a53bf7af4f547892260e0
Deniz Basocak <denizzr@outlook.com>
2022-01-08 23:51:42 +0300

    Some adjustments  were made.

--- lib/database/database_provider.dart
+++ lib/database/database_provider.dart
@@ -5,12 +5,14 @@ import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
 
 class DBHelper {
-  static const _TABLE_NAME = 'expenses';
+  static const _TABLE_NAME_EXPENSES = 'expenses';
+  static const _TABLE_NAME_INCOME = 'incomes';
 
   static Future<sql.Database> _database() async {
     final dbPath = await sql.getDatabasesPath();
+
     return sql.openDatabase(
-      path.join(dbPath, '$_TABLE_NAME.db'),
+      path.join(dbPath, '$_TABLE_NAME_EXPENSES.db'),
       onCreate: (db, version) {
         return db.execute(
             'CREATE TABLE expenses(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, amount REAL,desc TEXT ,date TEXT)');
@@ -20,9 +22,10 @@ class DBHelper {
   }
 
   static Future<void> insert(ExpenseModel data) async {
+    _TABLE_NAME_EXPENSES:
     final db = await DBHelper._database();
     db.insert(
-      _TABLE_NAME,
+      _TABLE_NAME_EXPENSES,
       {
         'title': data.type,
         'amount': data.amount,
@@ -34,6 +37,8 @@ class DBHelper {
 
   static Future<List<Map<String, dynamic>>> getData() async {
     final db = await DBHelper._database();
-    return db.query(_TABLE_NAME);
+    return db.query(_TABLE_NAME_EXPENSES);
   }
+
+  static Future<void> deleteData(String tableName, ExpenseModel data) async {}
 }
